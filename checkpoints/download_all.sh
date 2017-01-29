9c650d0f68abe2c49ad71fde50e2b208976863cb
cabaf <fabiancabaheilbron@gmail.com>
2017-01-29 19:34:30 +0300

    Script to download several checkpoint models.

---
+++ checkpoints/download_all.sh
@@ -0,0 +1,6 @@
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/January/587d1865_rain-princess/rain-princess.ckpt
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/January/588aa846_udnie/udnie.ckpt
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/January/588aa800_la-muse/la-muse.ckpt
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/January/588aa883_scream/scream.ckpt
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/January/588aa89d_wave/wave.ckpt
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/January/588aa8b6_wreck/wreck.ckpt
